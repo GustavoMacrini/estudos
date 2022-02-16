3059737e4062b7c77ef5c2d92f7f2e8ba96e57de
Gustavo <gustavo.macrini@hotmail.com>
2022-02-15 21:17:58 -0300

    Exercicios POO

---
+++ .Net C#/ExFixacaoEnum/ExFixacaoEnum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using ExFixacaoEnum.Entities;
+using ExFixacaoEnum.Entities.Enums;
+
+namespace ExFixacaoEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int qntdItems = int.Parse(Console.ReadLine());
+
+            Product pd;
+            OrderItem orderItem;
+            Order order = new Order(DateTime.Now, status, client);
+
+            for(int i = 0; i < qntdItems; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product name: ");
+                string pdName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double pdPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int pdQuantity = int.Parse(Console.ReadLine());
+
+                pd = new Product(pdName, pdPrice);
+                orderItem = new OrderItem(pdQuantity, pdPrice, pd);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
---
+++ .Net C#/ExFixacaoLista/ExFixacaoLista/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ExFixacaoLista
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+
+        public void aumentaSalario(double porcentagem)
+        {
+            Salario += (Salario * (porcentagem / 100)); 
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}"; 
+        }
+    }
+}
